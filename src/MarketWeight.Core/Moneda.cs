24b3fc8e5aa4f048aa3bde1072bc2091bc8df5a6
Walter-Cooking <walterbenitez00@gmail.com>
2025-07-16 23:51:41 -0300

    Commit final

--- src/MarketWeight.Core/Moneda.cs
+++ src/MarketWeight.Core/Moneda.cs
@@ -4,7 +4,6 @@ namespace MarketWeight.Core
     public class Moneda
     {
         public required decimal Precio { get; set; }
-        public required uint idMoneda { get; set; }
         public required decimal Cantidad { get; set; }
         public required string Nombre { get; set; }
     }
--- src/MarketWeight.Core/Usuario.cs
+++ src/MarketWeight.Core/Usuario.cs
@@ -2,7 +2,6 @@ namespace MarketWeight.Core;
 public class Usuario
 {
     public required string Nombre { get; set; }
-    public required uint idUsuario{get; set;}
     public required string Apellido { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
